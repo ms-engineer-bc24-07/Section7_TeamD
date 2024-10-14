8d28ea7d1a54ab73d0d84ae477bcf4dd40d139fd
akaneNK <rabiooron19@gmail.com>
2024-10-15 01:45:02 +0900

    エラーを一部解消した

--- frontend/app/routes/layout_all/_layout.tsx
+++ frontend/app/routes/layout_all/_layout.tsx
@@ -1,14 +1,13 @@
 // app/routes/_layout.tsx
 
-import { Header } from "./header";
-import { Footer } from "./footer";
+import Header from "./header";
+import Footer from "./footer";
 import { Outlet } from "@remix-run/react";
 
 export default function About() {
   return (
     <div>
       <Header />
-      <h1>This is About Route</h1>
       <Outlet />
       <Footer />
     </div>
